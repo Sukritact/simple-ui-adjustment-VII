031661809007d65cd3f5415c6bfd7f96f32f8a53
Sukrit Tan <actsukrit@gmail.com>
2025-02-12 23:29:06 -0500

    Initial Commit

---
+++ text/Suk_Simple_UI_Adjustments_Text.sql
@@ -0,0 +1,14 @@
+--========================================================================================================================
+--========================================================================================================================
+		INSERT OR REPLACE INTO LocalizedText
+			(Tag,	Language,	Text)
+		VALUES
+			('LOC_SUK_SUA_DISTRICT',		'en_US',	"{1_Type} District"),
+			('LOC_SUK_SUA_QUARTER',			'en_US',	"{1_Type} Quarter"),
+			('LOC_SUK_SUA_PLOT_OWNERSHIP',	'en_US',	"{1} {LOC_PLOT_DIVIDER_DOT} {2} ({3})"),
+			('LOC_SUK_SUA_IMPROVED_BY',		'en_US',	"Improve with: {1}"),
+			('LOC_SUK_SUA_OBSOLETE',		'en_US',	"Obsolete"),
+			('LOC_SUK_SUA_UNIMPROVED',		'en_US',	"Unimproved"),
+			('LOC_SUK_SUA_GROWTH_TT',		'en_US',	"{1}/{2}[icon:YIELD_Food] ({3}[icon:YIELD_Food])");
+--========================================================================================================================
+--========================================================================================================================
