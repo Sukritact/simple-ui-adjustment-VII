50672e6c2b5e892d9f2533dc9742982fcb7ddf1e
Sukrit Tan <actsukrit@gmail.com>
2025-02-19 21:28:26 -0500

    Added Translations for Spanish, Simplified Chinese, Italian, and Russian

--- text/Suk_Simple_UI_Adjustments_Text.sql
+++ text/Suk_Simple_UI_Adjustments_Text.sql
@@ -10,6 +10,42 @@
 			('LOC_SUK_SUA_IMPROVED_BY',		'en_US',	"Improve with: {1}"),
 			('LOC_SUK_SUA_OBSOLETE',		'en_US',	"Obsolete"),
 			('LOC_SUK_SUA_UNIMPROVED',		'en_US',	"Unimproved"),
-			('LOC_SUK_SUA_GROWTH_TT',		'en_US',	"{1}/{2}[icon:YIELD_Food] ({3}[icon:YIELD_Food])");
+			('LOC_SUK_SUA_GROWTH_TT',		'en_US',	"{1}/{2}[icon:YIELD_Food] ({3}[icon:YIELD_Food])"),
+			-- Spanish --
+			('LOC_SUK_SUA_YOUR_UNIT',		'es_ES',	"Tuyo"),
+			('LOC_SUK_SUA_DISTRICT',		'es_ES',	"Distrito {1_Type}"),
+			('LOC_SUK_SUA_QUARTER',			'es_ES',	"Barrio {1_Type}"),
+			('LOC_SUK_SUA_PLOT_OWNERSHIP',	'es_ES',	"{1} {LOC_PLOT_DIVIDER_DOT} {2} ({3})"),
+			('LOC_SUK_SUA_IMPROVED_BY',		'es_ES',	"Mejorado con: {1}"),
+			('LOC_SUK_SUA_OBSOLETE',		'es_ES',	"Obsoleto"),
+			('LOC_SUK_SUA_UNIMPROVED',		'es_ES',	"Sin mejoras"),
+			('LOC_SUK_SUA_GROWTH_TT',		'es_ES',	"{1}/{2}[icon:YIELD_Food] ({3}[icon:YIELD_Food])"),
+			-- Chinese (Simplified) --
+			('LOC_SUK_SUA_YOUR_UNIT',		'zh_Hans_CN',	"你的"),
+			('LOC_SUK_SUA_DISTRICT',		'zh_Hans_CN',	"{1_Type}区块"),
+			('LOC_SUK_SUA_QUARTER',			'zh_Hans_CN',	"{1_Type}城区"),
+			('LOC_SUK_SUA_PLOT_OWNERSHIP',	'zh_Hans_CN',	"{1} {LOC_PLOT_DIVIDER_DOT} {2} ({3})"),
+			('LOC_SUK_SUA_IMPROVED_BY',		'zh_Hans_CN',	"预期的改良设施: {1}"),
+			('LOC_SUK_SUA_OBSOLETE',		'zh_Hans_CN',	"过时"),
+			('LOC_SUK_SUA_UNIMPROVED',		'zh_Hans_CN',	"无改良设施"),
+			('LOC_SUK_SUA_GROWTH_TT',		'zh_Hans_CN',	"{1}/{2}[icon:YIELD_Food] ({3}[icon:YIELD_Food])"),
+			-- Italian --
+			('LOC_SUK_SUA_YOUR_UNIT',		'it_IT',	"Tuoi"),
+			('LOC_SUK_SUA_DISTRICT',		'it_IT',	"Distretto {1_Type}"),
+			('LOC_SUK_SUA_QUARTER',			'it_IT',	"Quartiere {1_Type}"),
+			('LOC_SUK_SUA_PLOT_OWNERSHIP',	'it_IT',	"{1} {LOC_PLOT_DIVIDER_DOT} {2} ({3})"),
+			('LOC_SUK_SUA_IMPROVED_BY',		'it_IT',	"Migliorato con: {1}"),
+			('LOC_SUK_SUA_OBSOLETE',		'it_IT',	"Obsoleto"),
+			('LOC_SUK_SUA_UNIMPROVED',		'it_IT',	"Non migliorato"),
+			('LOC_SUK_SUA_GROWTH_TT',		'it_IT',	"{1}/{2}[icon:YIELD_Food] ({3}[icon:YIELD_Food])"),
+			-- Russian --
+			('LOC_SUK_SUA_YOUR_UNIT',		'ru_RU',	"ваш"),
+			('LOC_SUK_SUA_DISTRICT',		'ru_RU',	"{1_Type} Район"),
+			('LOC_SUK_SUA_QUARTER',			'ru_RU',	"{1_Type} Квартал"),
+			('LOC_SUK_SUA_PLOT_OWNERSHIP',	'ru_RU',	"{1} {LOC_PLOT_DIVIDER_DOT} {2} ({3})"),
+			('LOC_SUK_SUA_IMPROVED_BY',		'ru_RU',	"Улучшает: {1}"),
+			('LOC_SUK_SUA_OBSOLETE',		'ru_RU',	"Устарело"),
+			('LOC_SUK_SUA_UNIMPROVED',		'ru_RU',	"Не улучшено"),
+			('LOC_SUK_SUA_GROWTH_TT',		'ru_RU',	"{1}/{2}[icon:YIELD_Food] ({3}[icon:YIELD_Food])");
 --========================================================================================================================
 --========================================================================================================================
